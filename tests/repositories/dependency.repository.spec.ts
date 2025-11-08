3bdc7c6117e69fe21d51e0c75c68ce6eb4beb390
Dan Ruan <danrsaquetti@gmail.com>
2025-11-08 14:18:52 -0300

    feat: Criando teste de registro simples da depencia no repositório

---
+++ tests/repositories/dependency.repository.spec.ts
@@ -0,0 +1,24 @@
+import { DependencyRepository } from '@repositories/dependency.repository'
+import { beforeEach, describe, expect, test } from 'vitest'
+import { Dependency } from '../../src/common/types/types'
+import { Scope } from '../../src/enums/scope'
+
+describe('Dependency Repository Tests', () => {
+  let repository: DependencyRepository
+
+  beforeEach(() => {
+    repository = new DependencyRepository()
+  })
+
+  test('Expected to register a Dependency', () => {
+    const dependency: Dependency = {
+      token: 'TOKEN',
+      scope: Scope.REQUEST,
+      useValue: 10
+    }
+
+    repository.register(dependency)
+
+    expect(repository.get(dependency.token)).toEqual(dependency)
+  })
+})
